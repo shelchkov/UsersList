d644b73bc7615430a0477326a4b9b87745a652a6
shelchkovy <shelchkovy@yandex.ru>
2020-05-16 10:09:04 +0400

    Refactor main.js #99

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 // Link to API
-const link = "https://api.randomuser.me/1.0/?results=50&nat=gb,us&inc=gender,name,location,email,phone,picture";
+const link = "https://api.randomuser.me/1.0/?results=50&nat=gb,us&inc=" +
+	"gender,name,location,email,phone,picture";
 
-// User's List
 let usersList = [];
 
 
@@ -71,7 +71,8 @@ const icons = {
 	}
 };
 
-const getUserName = (user) => `${user.name.title} ${user.name.first} ${user.name.last}`;
+const getUserName = (user) =>
+	`${user.name.title} ${user.name.first} ${user.name.last}`;
 const getUserLastName = (user) => `${user.name.title} ${user.name.last}`;
 
 
@@ -91,8 +92,9 @@ function updateContent() {
 	usersList.forEach(function(user, i) {
 		let user_html = 
 			`<article class="user user--${i}" title="Show More Info">`;
-		user_html += `<div class="user--placeholder"><img class="user--image"
-			src=${user.picture.large} alt="${getUserLastName(user)}"></div>`;
+		user_html += `<div class="user--placeholder">
+			<img class="user--image" src=${user.picture.large}
+			alt="${getUserLastName(user)}"></div>`;
 		user_html += `<div class="user--info">
 			<h4 class="user--info--name">${getUserName(user)}</h4>
 			<p class="user--info--show-more">Click To See More</p></div>`;
@@ -106,8 +108,10 @@ function updateContent() {
 	let userCards = document.querySelectorAll(".user");
 	for(let i = 0; i < userCards.length; i++) {
 		userCards[i].onclick = () => showInfo(usersList[i]);
-		userCards[i].onmouseenter = () => userCardInfoToggle(userCards[i]);
-		userCards[i].onmouseleave = () => userCardInfoToggle(userCards[i]);
+		userCards[i].onmouseenter = () =>
+			userCardInfoToggle(userCards[i]);
+		userCards[i].onmouseleave = () =>
+			userCardInfoToggle(userCards[i]);
 	}
 
 	// Make div.sort visible
@@ -122,10 +126,12 @@ function showInfo(user) {
 	const infoBlock = document.querySelector(".modal--info");
 	userInfo += `<p class="modal--closeModal modal--closeButton" 
 		title="Close">X</p>`;
-	userInfo += `<img src=${user.picture.large} alt="${getUserLastName(user)}">`;
+	userInfo +=
+		`<img src=${user.picture.large} alt="${getUserLastName(user)}">`;
 	const location = `${user.location.street}, ${user.location.city}, 
 		${user.location.state}`;
-	userInfo += `<div class="modal--info--text"><p><img class="icon" src=${icons.location.url} 
+	userInfo += `<div class="modal--info--text"><p><img class="icon"
+		src=${icons.location.url} 
 		onerror="this.src='${icons.location.fallback}'">${location}</p>`;
 	userInfo += `<p><img class="icon" src=${icons.email.url}
 		onerror="this.src='${icons.email.fallback}'">${user.email}</p>`;
